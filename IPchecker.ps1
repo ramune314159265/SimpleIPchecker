eb5a7d168bd45275cf5242d9f457b9fc1ce9ab91
ramune314159265 <ramune314159265@gmail.com>
2024-02-19 17:53:59 +0900

    IPを記録し変更があったか検知する

---
+++ IPchecker.ps1
@@ -0,0 +1,16 @@
+$IP_FILE_PATH = Join-Path $env:TEMP '/globalip.txt'
+
+$presentGlobalIP = (Invoke-WebRequest https://ipinfo.io/ip).content
+
+if(!(Test-Path $IP_FILE_PATH)){
+	New-Item $IP_FILE_PATH -type file
+	Set-Content -Path $IP_FILE_PATH -Value $presentGlobalIP
+
+	exit
+}
+
+$previousGlobalIP = (Get-Content $IP_FILE_PATH -Encoding UTF8)
+
+if(!($presentGlobalIP -eq $previousGlobalIP)){
+	Write-Output a
+}
