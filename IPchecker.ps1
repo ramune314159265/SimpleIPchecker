95c2c3d570f63fba629ff827165c40f15900cee8
ramune314159265 <ramune314159265@gmail.com>
2024-02-19 18:01:48 +0900

    変更があったら通知するように

--- IPchecker.ps1
+++ IPchecker.ps1
@@ -1,3 +1,4 @@
+﻿$OutputEncoding = [Text.UTF8Encoding]::UTF8
 $IP_FILE_PATH = Join-Path $env:TEMP '/globalip.txt'
 
 $presentGlobalIP = (Invoke-WebRequest https://ipinfo.io/ip).content
@@ -12,7 +13,12 @@ if(!(Test-Path $IP_FILE_PATH)){
 $previousGlobalIP = (Get-Content $IP_FILE_PATH -Encoding UTF8)
 
 if(!($presentGlobalIP -eq $previousGlobalIP)){
-	Write-Output a
+	$Toast = [Windows.UI.Notifications.ToastTemplateType, Windows.UI.Notifications, ContentType = WindowsRuntime]::ToastText02
+	$ToastContent = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::GetTemplateContent($Toast)
+	$ToastContent.SelectSingleNode('//text[@id="1"]').InnerText = 'グローバルIPアドレスに変更がありました'
+	$ToastContent.SelectSingleNode('//text[@id="2"]').InnerText = $presentGlobalIP
+	$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+	[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).Show($ToastContent)
 
 	Set-Content -Path $IP_FILE_PATH -Value $presentGlobalIP
 }
